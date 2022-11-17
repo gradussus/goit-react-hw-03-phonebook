09df595c66f8915a298ae8e1259dffc5980b58b0
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2022-11-17 14:19:17 +0200

    майже

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,13 +16,26 @@ const Container = styled.section`
 export class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: nanoid(10), name: 'Rosie Simpson', number: '459-12-56' },
+      { id: nanoid(10), name: 'Hermione Kline', number: '443-89-12' },
+      { id: nanoid(10), name: 'Eden Clements', number: '645-17-79' },
+      { id: nanoid(10), name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
+
+  componentDidMount() {
+    if (localStorage.getItem('contacts')) {
+      this.setState({ contacts: JSON.parse(localStorage.getItem('contacts')) })
+    }
+}
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts',JSON.stringify(this.state.contacts))
+    }
+  }
+  
   addContact = ({ name, number }) => {
     const includeName = name => {
       return this.state.contacts.find(
@@ -79,6 +92,7 @@ export class App extends Component {
         <ContactForm onSubmit={this.addContact} />
         <h2>Contacts</h2>
         <Filter value={this.filter} changeFilter={this.changeFilter} />
+        {}
         <ContactList
           contacts={this.filtredContacts()}
           deleteCont={this.deleteContact}
